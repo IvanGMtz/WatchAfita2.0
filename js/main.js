80711ff2e90a6f88a546addac09c2fd8b9066496
IvanGMtz <navi.garces24@gmail.com>
2023-05-21 15:44:40 -0500

    style: :lipstick: Style card
    Size of the description to be inside the card

--- js/main.js
+++ js/main.js
@@ -48,15 +48,15 @@ function idPeli(peli) {
 function contruirPeli(peli){
 
         document.getElementById("OrderbyRanking").innerHTML += `<div class="card mb-3 mt-2 ms-3 border-light bg-dark text-light g-0" style="width: 300px; height:  360px;">
-        <div class="row text-center "><h5 class="card-title g-0 p-2">${peli.results.title} ⭐${peli.results.rating}</h5></div>
+        <div class="row text-center "><h5 class="card-title g-0 p-2 ">${peli.results.title} ⭐${peli.results.rating}</h5></div>
         <div class="row g-0 align-items-center">
           <div class="col-md " style="display: flex; height: 28vh; overflow: auto; flex-direction: column;">
             <img src="${peli.results.image_url}" style="width: 100%; height: 30vh;" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md">
-            <div class="card-body" style="display: flex;height: 300px; overflow: auto; flex-direction: column;">
-              <p class="card-text">${peli.results.description}</p>
-              <p class="card-text"><small class="text-body-light">Ver Trailer</small></p>
+            <div class="card-body" style="display: flex;height: 280px; overflow: auto; flex-direction: column;">
+              <p class="card-text text-justify">${peli.results.description}</p>
+              <p class="card-text"><small class="text-body-light"><a href="${peli.results.trailer}" target="_blank">Ver Trailer</a></small></p>
             </div>
           </div>
         </div>
