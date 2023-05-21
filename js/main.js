46d58a691cfbd0b53e44bdbc84dfc61d805657b3
IvanGMtz <navi.garces24@gmail.com>
2023-05-21 16:28:38 -0500

    feat: :zap: Introduce Event
    new function to optain movies by name

--- js/main.js
+++ js/main.js
@@ -18,8 +18,36 @@ const GetAll = async () => {
         console.error(error);
     }
 }
+document.addEventListener("click", async e => {
+    if (e.target.matches("#boton")) {
+        document.getElementById("OrderbyRanking").innerHTML ="";
+        let name = document.getElementById("buscador").value;
+        document.getElementById("titulo").innerHTML=`<p class="h3 text-light">Peliculas sugeridas por ${name}</p>`
+        let url = `https://moviesminidatabase.p.rapidapi.com/movie/imdb_id/byTitle/${name}/`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'aaae3ef658msh451c8cdc3dc7901p195a71jsna3ed1b1c302a',
+                'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+            }
+        };
 
-GetAll();
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result);
+            idPeli(result);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    if (e.target.matches(".title")){
+        document.getElementById("OrderbyRanking").innerHTML ="";
+        document.getElementById("titulo").innerHTML=`<p class="h3 text-light">Peliculas con mejor Puntuación</p>`
+        GetAll();
+    } 
+});
+
 
 const infoPeli = async (e) => {
     let url = `https://moviesminidatabase.p.rapidapi.com/movie/id/${e}/`;
@@ -36,18 +64,19 @@ const infoPeli = async (e) => {
         const result = await response.json();
         console.log(result);
         contruirPeli(result);
-        
+
     } catch (error) {
         console.error(error);
-    }}
-
-function idPeli(peli) {
-    peli.results.forEach(e => {infoPeli(e.imdb_id);});
+    }
 }
 
-function contruirPeli(peli){
+function idPeli(peli) {
+    peli.results.forEach(e => { infoPeli(e.imdb_id); });
+}
 
-        document.getElementById("OrderbyRanking").innerHTML += `<div class="card mb-3 mt-2 ms-3 border-light bg-dark text-light g-0" style="width: 300px; height:  360px;">
+function contruirPeli(peli) {
+
+    document.getElementById("OrderbyRanking").innerHTML += `<div class="card mb-3 mt-2 ms-3 border-light bg-dark text-light g-0" style="width: 300px; height:  360px;">
         <div class="row text-center "><h5 class="card-title g-0 p-2 ">${peli.results.title} ⭐${peli.results.rating}</h5></div>
         <div class="row g-0 align-items-center">
           <div class="col-md " style="display: flex; height: 28vh; overflow: auto; flex-direction: column;">
@@ -61,5 +90,5 @@ function contruirPeli(peli){
           </div>
         </div>
     </div>`;
-    
+
 }
